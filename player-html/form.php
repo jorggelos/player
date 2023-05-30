6859ef24d938f8f3cd75104b312030221030fed4
unknown <jl.jorgelos@hotmail.com>
2023-05-30 16:46:14 -0300

    update

---
+++ player-html/form.php
@@ -0,0 +1,16 @@
+<form method="get" action="envio_dados.php">
+    <input type="text" name="nome" placeholder="digite seu Nome aqui">
+    <input type="e-mail" name="email" placeholder="Digite seu e-mail">
+    <input type="submit" name="enviar" value="Enviar">    
+</form>
+
+
+
+<div id="content" class="lightbox">
+  <form id="form_1">
+      <input type="text" value="Nome"/><br/>
+      <input type="text" value="Email"/>
+      <input type="submit" value="enviar"/>
+      <a class="close" href="#">X</a> 
+  </form>
+</div>
